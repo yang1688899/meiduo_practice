8026bef0ccc390c0bea967b38f81f0b471ff707c
yang <yang535460548484@gmail.com>
2023-07-30 20:24:22 +0800

    RORS setting

--- meidou_mall/meidou_mall/settings.py
+++ meidou_mall/meidou_mall/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'apps.users'
+    'corsheaders',#跨域cors
+
+    'apps.users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',#跨域cors
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -194,3 +198,13 @@ LOGGING = {
 
 ##################
 AUTH_USER_MODEL = 'users.User'
+
+
+#######CORS#######
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+    'http://www.meiduo.site:8080',
+    'http://www.meiduo.site:8000'
+)
+CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
