572f8b9fa53900a5b5d1919e5ba1003c3e751ebb
yang <yang535460548484@gmail.com>
2023-08-03 20:17:51 +0800

    phone validate function

--- meidou_mall/apps/captcha/views.py
+++ meidou_mall/apps/captcha/views.py
@@ -24,17 +24,27 @@ class MessageValidView(View):
         img_code_cli = request.GET.get('image_code').lower()
         uuid = request.GET.get('image_code_id')
 
+        redis_cli = get_redis_connection('captcha')
+
+        send_flag = redis_cli.get('send_flag_%s'%mobile)
+        if send_flag:
+            return JsonResponse({'code':400, 'errmsg':'验证请求过于频繁!!!'})
+
         if not all([mobile, img_code_cli, uuid]):
             return JsonResponse({'code': 400, 'errmsg': '参数不全!!!'})
 
-        redis_cli = get_redis_connection('captcha')
-        img_code_server = redis_cli.get(uuid).decode().lower()
+        img_code_server = redis_cli.get(uuid)
+        if not img_code_server:
+            return JsonResponse({'code': 400, 'errmsg': '图形验证码已过期!!!'})
 
-        if not img_code_server == img_code_cli:
+        if not img_code_server.decode().lower() == img_code_cli:
             return JsonResponse({'code': 400, 'errmsg': '图形验证码错误!!!'})
 
         ##### pretend to send a message ######
         message_code = '123456'
-        redis_cli.setex(name=mobile, value=message_code, time=300)
+        pipeline = redis_cli.pipeline()
+        pipeline.setex(name=mobile, value=message_code, time=300)
+        pipeline.setex(name='send_flag_%s'%mobile, value=1, time=120)
+        pipeline.execute()
 
         return JsonResponse({'code': 0, 'errmsg': 'ok'})
