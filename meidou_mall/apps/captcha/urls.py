6998a6105c48075d10dc51c4b2c23d85c400975c
yang <yang535460548484@gmail.com>
2023-08-03 15:32:21 +0800

    phone validate function

--- meidou_mall/apps/captcha/urls.py
+++ meidou_mall/apps/captcha/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from apps.captcha import views
 
 urlpatterns = [
-    path('image_codes/<uuid>/', views.ImageCaptchaView.as_view())
+    path('image_codes/<uuid>/', views.ImageCaptchaView.as_view()),
+    path('sms_codes/<mobile>/', views.MessageValidView.as_view()),
+
 ]
--- meidou_mall/apps/captcha/views.py
+++ meidou_mall/apps/captcha/views.py
@@ -1,21 +1,40 @@
-from django.http import HttpResponse
+import django_redis
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 from django_redis import get_redis_connection
 
-
 from libs.captcha.captcha import captcha
 
 
 class ImageCaptchaView(View):
     def get(self, request, uuid):
-
         code, captcha_img_byte = captcha.generate_captcha()
         redis_cli = get_redis_connection('captcha')
-        redis_cli.setex(name=uuid, time=120, value=code)
+        redis_cli.setex(name=uuid, time=120, value=code.lower())
 
         return HttpResponse(captcha_img_byte, content_type='image/jpeg')
 
 
+class MessageValidView(View):
+    def get(self, request, mobile):
+
+        img_code_cli = request.GET.get('image_code').lower()
+        uuid = request.GET.get('image_code_id')
+
+        if not all([mobile, img_code_cli, uuid]):
+            return JsonResponse({'code': 400, 'errmsg': '参数不全!!!'})
+
+        redis_cli = get_redis_connection('captcha')
+        img_code_server = redis_cli.get(uuid).decode().lower()
+
+        if not img_code_server == img_code_cli:
+            return JsonResponse({'code': 400, 'errmsg': '图形验证码错误!!!'})
+
+        ##### pretend to send a message ######
+        message_code = '123456'
+        redis_cli.setex(name=mobile, value=message_code, time=300)
+
+        return JsonResponse({'code': 0, 'errmsg': 'ok'})
