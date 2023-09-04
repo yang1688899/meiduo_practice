c76a7cf88b0959464f9ad5eb22c34b4b7cd861a3
yang <yang535460548484@gmail.com>
2023-09-04 14:59:25 +0800

    regist and login function

--- meidou_mall/apps/users/urls.py
+++ meidou_mall/apps/users/urls.py
@@ -3,5 +3,7 @@ from apps.users import views
 
 urlpatterns = [
     path('usernames/<username_valid:username>/count/', views.UserCountView.as_view()),
+    path('mobiles/<mobile>/count/', views.MobileCountView.as_view()),
     path('register/', views.RegisterView.as_view()),
+    path('login/', views.LoginView.as_view()),
 ]
--- meidou_mall/apps/users/views.py
+++ meidou_mall/apps/users/views.py
@@ -1,6 +1,7 @@
 import json
+import re
 
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.hashers import make_password
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -14,6 +15,13 @@ class UserCountView(View):
 
     def get(self, request, username):
         count = User.objects.filter(username=username).count()
+        return JsonResponse({'code': 0, 'count': count, 'errmsg': 'ok'})
+
+
+class MobileCountView(View):
+
+    def get(self, request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
         return JsonResponse({'code': 0, 'count': count, 'errmsg': 'ok'})
 
 
@@ -38,3 +46,46 @@ class RegisterView(View):
         login(request, user)
 
         return JsonResponse({'code': 0, 'errmsg': 'ok'})
+
+
+class LoginView(View):
+
+    def post(self, request):
+
+        json_str = request.body.decode()
+        data_dict = json.loads(json_str)
+
+        username = data_dict.get('username')
+        password = data_dict.get('password')
+        remembered = data_dict.get('remembered')
+
+        #data validation
+
+        if not all([username,password]):
+            return JsonResponse({'code':400, 'errmsg':'用户名或密码错误!!!'})
+
+        if re.fullmatch('1[345789]\d{9}', username):
+            User.USERNAME_FIELD = 'mobile'
+        else:
+            User.USERNAME_FIELD = 'username'
+        user = authenticate(username=username,password=password)
+
+        if not user:
+            return JsonResponse({'code': 400, 'errmsg': '用户名或密码错误!!!'})
+
+        login(request, user)
+
+        if remembered:
+            request.session.set_expiry(None)
+
+        else:
+            request.session.set_expiry(0)
+
+        response = JsonResponse({'code':0, 'errmsg':'ok'})
+        response.set_cookie('username', username)
+
+        return response
+
+
+
+
